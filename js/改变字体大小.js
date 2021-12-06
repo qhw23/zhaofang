87c9cc39ed6ec5c40aefaf318f43c198e8ccb7f6
qhw23 <2650064822@qq.com>
2021-12-06 18:43:15 +0800

    '第一天'

---
+++ js/改变字体大小.js
@@ -0,0 +1,15 @@
+
+    function setRem() {
+        var ui_w = 375;
+        // 获取屏幕的宽度
+        var clientWidth = document.documentElement.clientWidth || document.body.clientWidth;
+        console.log(ui_w,clientWidth);
+
+        // 通过js动态改变html根节点字体大小
+        var html_ = document.getElementsByTagName('html')[0];
+        html_.style.fontSize = (clientWidth/ui_w)*10 +'px';   //625%是乘100     62.5%就是乘10 
+    }
+    // window.onresize 浏览器被重置大小执行事件
+    window.onresize = setRem;
+
+
